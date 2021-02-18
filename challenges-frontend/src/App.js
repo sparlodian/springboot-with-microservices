aff68a9bef6f5c19113d77ea6c04bf6de1e8c923
Henri Lehtinen <henri.a.lehtinen@outlook.com>
2021-02-18 12:15:27 +0200

    Frontend react starter code

---
+++ challenges-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ChallengeComponent from './components/ChallengeComponent.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChallengeComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ challenges-frontend/src/services/api-client.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+class ApiClient {
+
+    static SERVER_URL = 'http://localhost:8080';
+    static GET_CHALLENGE = '/challenges/random';
+    static POST_RESULT = '/attempts';
+
+    static challenge() {
+        return fetch(ApiClient.SERVER_URL + ApiClient.GET_CHALLENGE);
+    }
+
+    static sendGuess(user,
+                     a,
+                     b,
+                     guess){
+        return fetch(ApiClient.SERVER_URL + ApiClient.POST_RESULT,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        userAlias: user,
+                        factorA: a,
+                        factorB: b,
+                        guess: guess
+                    }
+                )
+            });
+    }
+}
+
+export default ApiClient;
